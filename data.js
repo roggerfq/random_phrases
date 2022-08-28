9a576e74a04f14bfb7e138623ff9476051a465e1
roggerfq <36053387+roggerfq@users.noreply.github.com>
2022-08-28 16:30:48 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,26 @@
+data = [
+  {
+    "url": "https://www.youtube.com/embed/uBVCUSkoBIM?autoplay=1&mute=0&enablejsapi=1",
+    "start": "0",
+    "end": "7",
+    "text": "With the new revelations relating to the FBI search of moral Lago, President Biden's decision to cancel some student debt and primary elections held earlier this week"
+  },
+  {
+    "url": "https://www.youtube.com/embed/th3KE_H27bs?autoplay=1&mute=0&enablejsapi=1",
+    "start": "33",
+    "end": "40",
+    "text": "Today, I have come to share the secrets of our success, because rich capitalists like me have never been richer."
+  },
+  {
+    "url": "https://www.youtube.com/embed/XgRlrBl-7Yg?autoplay=1&mute=0&enablejsapi=1",
+    "start": "161",
+    "end": "168",
+    "text": "because he was left with a memory; the memory was ruined, and the memory was all that he had gotten to keep."
+  },
+  {
+    "url": "https://www.youtube.com/embed/Yhn1Fe8cT0Q?autoplay=1&mute=0&enablejsapi=1",
+    "start": "26",
+    "end": "32",
+    "text": "He was doing very well until a shipwreck destroyed everything and he lost everything he had."
+  }
+];
