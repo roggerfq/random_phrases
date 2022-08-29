96eff646523e6e940b4827cf7a95652c113cfbf4
roggerfq <36053387+roggerfq@users.noreply.github.com>
2022-08-28 20:55:38 -0500

    Update data.js

--- data.js
+++ data.js
@@ -22,5 +22,35 @@ data = [
     "start": "26",
     "end": "32",
     "text": "He was doing very well until a shipwreck destroyed everything and he lost everything he had."
+  },
+  {
+    "url": "https://www.youtube.com/embed/5c6C3rHOdf8?autoplay=1&mute=0&enablejsapi=1",
+    "start": "40",
+    "end": "49",
+    "text": "which claimed that living things were unique because they were filled with a special substance or energy that was the essence of life"
+  },
+  {
+    "url": "https://www.youtube.com/embed/5c6C3rHOdf8?autoplay=1&mute=0&enablejsapi=1",
+    "start": "148",
+    "end": "156",
+    "text": "the universal phenomenon of entropy means that molecules will tend towards diffusing randomly"
+  },
+  {
+    "url": "https://www.youtube.com/embed/mAFv55o47ok?autoplay=1&mute=0&enablejsapi=1",
+    "start": "32",
+    "end": "37",
+    "text": "we spoke to the 44th president on the release of his new book"
+  },
+  {
+    "url": "https://www.youtube.com/embed/mAFv55o47ok?autoplay=1&mute=0&enablejsapi=1",
+    "start": "249",
+    "end": "260",
+    "text": "we have gone through a precidency that disregarder a whole host of basic institucional norms"
+  },
+  {
+    "url": "https://www.youtube.com/embed/6NaS2HHeGvM?autoplay=1&mute=0&enablejsapi=1",
+    "start": "141",
+    "end": "146",
+    "text": "I don't know if I gonna meet someone and they've seem this pictures are they judging me because of this pictures"
   }
 ];
